2c161c3958b3438d61b9591721f14c883c4fbd53
Calle Helmertz <helmertz@gmail.com>
2024-01-17 17:51:48 +0100

    empty result

--- internal/partille/search_test.go
+++ internal/partille/search_test.go
@@ -1,23 +1,28 @@
 package partille
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
 	"golang.org/x/net/html"
 )
 
-func TestCanParseResult(t *testing.T) {
-	file, err := os.Open("search_result_example.html")
+func testFile(filename string) *html.Node {
+	file, err := os.Open(filename)
 	if err != nil {
-		t.Fatalf("failed to read file: %v", err)
+		panic(fmt.Sprintf("failed to read file: %v", err))
 	}
 
 	node, err := html.Parse(file)
 	if err != nil {
-		t.Fatalf("failed to parse html: %v", err)
+		panic(fmt.Sprintf("failed to parse html: %v", err))
 	}
+	return node
+}
 
+func TestCanParseResult(t *testing.T) {
+	node := testFile("search_result_example.html")
 	result := firstResult(node)
 	if result == nil {
 		t.Fatalf("expected result, got nil")
@@ -31,5 +36,13 @@ func TestCanParseResult(t *testing.T) {
 	expectedUrl := "https://bibliotekskatalog.partille.se/cgi-bin/koha/opac-detail.pl?biblionumber=10330"
 	if result.DetailUrl != expectedUrl {
 		t.Errorf("expected url to be %s, got %s", expectedUrl, result.DetailUrl)
+	}
+}
+
+func TestCanRepresentEmptyResult(t *testing.T) {
+	node := testFile("search_result_empty_example.html")
+	result := firstResult(node)
+	if result != nil {
+		t.Fatalf("expected result, got nil")
 	}
 }
